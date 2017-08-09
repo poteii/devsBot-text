3f7238284cbeeb784ebeee216b6fa0e4085a245d
poteii <poteii.pasel@icloud.com>
2017-08-09 15:30:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			if($event['message']['text'] == "สวัสดี"){
 				// Get text sent
-				$text = "สวัสดีค่ะ คุณ ".$event['source']['userId'];
+				$text = "สวัสดีค่ะ คุณ ".$event['source']['userName'];
 			
 			}else{
 			// Get text sent
