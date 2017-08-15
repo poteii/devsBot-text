0a03c9000b165279b20fad9396fa87961a6add1e
littlesquid <littlesquid.1991@gmail.com>
2017-08-15 14:21:45 +0700

    Update firebase.php

--- firebase.php
+++ firebase.php
@@ -2,8 +2,10 @@
 $url = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
 $arr = array("TEST" =>array("test"=>array("a"=>123)));  
 $data_string = json_encode($arr);
+
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt( $ch, CURLOPT_URL, $FIREBASE . "FirstBase.json" );
+/*curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
@@ -11,6 +13,6 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 'Content-Type: application/json',
 'Content-Length: ' . strlen($data_string))
-);
+);*/
 $result = curl_exec($ch);
 ?>
