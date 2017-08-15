daf0b0c527c15faaafd04bf338e19e6bc4748d49
littlesquid <littlesquid.1991@gmail.com>
2017-08-15 12:23:24 +0700

    Update firebase.php

--- firebase.php
+++ firebase.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'https://myfirstfirebase-3f424.firebaseio.com/Devices.json';
+$url = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
 $arr = array("TEST" =>array("iPhone"=>500));  
 $data_string = json_encode($arr);
 $ch = curl_init($url);
