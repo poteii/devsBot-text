1a8ce68ba792870d9189f66b1dce00c18776599f
littlesquid <littlesquid.1991@gmail.com>
2017-08-15 12:53:52 +0700

    Update firebase.php

--- firebase.php
+++ firebase.php
@@ -3,7 +3,7 @@ $url = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
 $arr = array("TEST" =>array("test"=>array("a"=>123)));  
 $data_string = json_encode($arr);
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
