e5c38dc8427495e925cd939f360ff537e02721e7
littlesquid <littlesquid.1991@gmail.com>
2017-08-15 11:35:42 +0700

    Add files via upload

---
+++ firebase.php
@@ -0,0 +1,54 @@
+<?php
+
+// Constants
+$FIREBASE = "https://myfirstfirebase-3f424.firebaseio.com";
+$NODE_DELETE = "temperature.json";
+$NODE_GET = "temperature.json";
+$NODE_PATCH = ".json";
+$NODE_PUT = "temperature.json";
+
+// Data for PUT
+// Node replaced
+$data = 32;
+
+// Data for PATCH
+// Matching nodes updated
+$data = array(
+    "temperature" => 42
+);
+
+// JSON encoded
+$json = json_encode( $data );
+
+// Initialize cURL
+$curl = curl_init();
+
+// Create
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PUT );
+// curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PUT" );
+// curl_setopt( $curl, CURLOPT_POSTFIELDS, 32 );
+
+// Read
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_GET );
+
+// Update
+curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+
+// Delete
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_DELETE );
+// curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "DELETE" );
+
+// Get return value
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+
+// Make request
+// Close connection
+$response = curl_exec( $curl );
+curl_close( $curl );
+
+// Show result
+echo $response . "\n";
+
+?>
