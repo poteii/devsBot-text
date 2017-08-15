de1c5c86b9df1ccecb8f742064f0c7500624804f
poteii <poteii.pasel@icloud.com>
2017-08-15 12:33:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -54,7 +54,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			echo $result . "\r\n";
+			$result . "\r\n";
 			
 			
 			$firebaseurl = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
