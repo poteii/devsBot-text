629d0b720ac09a9c300c952fe4d82540fcad5cf1
littlesquid <littlesquid.1991@gmail.com>
2017-08-16 16:51:28 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,14 +1,24 @@
 <?php
+include 'input.php';
+include 'firebase.php';
 $access_token = 'nh7BWpnKFdxiz9UTcB3HttsbbBC9DIxMHeQUGznWDqLQ6yAyM9iyYSqn6BO4Yg+6NxNqZA3ZDmcu/1O7RTO0SL4vUdQdoTgOkQCVJP8Qm5O2ivj40/ezbv/n51ekqhaFUGl47j8Yvx8ChjaEhfH/DAdB04t89/1O/w1cDnyilFU=';
-$userId = $_GET('userId');
+
 $messages = [
 				'type' => 'text',
 				'text' => "Hello, world"
 			];
 // Make a POST Request to Messaging API to reply to sender
+
+
+$returl = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
+$retch = curl_init($returl);
+curl_setopt( $retch, CURLOPT_RETURNTRANSFER, true );
+$result = curl_exec($retch);
+$receivers = regenerateArray($result);
+foreach($receivers as $key => $value){
     $url = 'https://api.line.me/v2/bot/message/push';
     $data = [
-        'to' => $userId,
+        'to' => $key,
         'messages' => [$messages],
     ];
     $post = json_encode($data);
@@ -22,4 +32,5 @@ $messages = [
     $result = curl_exec($ch);
     curl_close($ch);
     echo $result . "\r\n";
+}
 ?>
