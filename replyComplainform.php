43f868f1d69351bb696b26fbf502362a08c8f12c
poteii <poteii.pasel@icloud.com>
2017-08-16 17:19:49 +0700

    Update replyComplainform.php

--- replyComplainform.php
+++ replyComplainform.php
@@ -1,6 +1,13 @@
 
 <?php
 include 'complainform.php';
+
+$access_token = 'nh7BWpnKFdxiz9UTcB3HttsbbBC9DIxMHeQUGznWDqLQ6yAyM9iyYSqn6BO4Yg+6NxNqZA3ZDmcu/1O7RTO0SL4vUdQdoTgOkQCVJP8Qm5O2ivj40/ezbv/n51ekqhaFUGl47j8Yvx8ChjaEhfH/DAdB04t89/1O/w1cDnyilFU=';
+$messages = [
+				'type' => 'text',
+				'text' => "ขอบคุณสำหรับข้อมูล ทางเราจะรีบดำเนินการโดยเร็วที่สุด"
+			];
+
 $fburl = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
 $fbarr = array($userId => array($subject => $detail));  
 $fbdata_string = json_encode($fbarr);
@@ -15,4 +22,32 @@ curl_setopt($fbch, CURLOPT_HTTPHEADER, array(
 'Content-Length: ' . strlen($fbdata_string))
 );
 $fbresult = curl_exec($fbch);
+
+
+
+
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/message/push';
+$data = [
+    'to' => $userId ,
+    'messages' => [$messages],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
+
+
+
+
+
+
+
 ?>
