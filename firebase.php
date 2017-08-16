58c5096697cbd6954c8508cde1c6d1c22929361f
littlesquid <littlesquid.1991@gmail.com>
2017-08-16 13:36:57 +0700

    Update firebase.php

--- firebase.php
+++ firebase.php
@@ -21,7 +21,7 @@ foreach($arr_result as $key => $value){
   //print_r($value);
   $arr_result1 = explode(":",$value);
   //print_r($arr_result1);
-  foreach($arr_result1 => $value1){
+  foreach($arr_result1 as $value1){
     print_r($value1);
   }
 }
