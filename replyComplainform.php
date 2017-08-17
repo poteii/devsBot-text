11901095a1198b7ee07387a0846cd32c12832bbd
poteii <poteii.pasel@icloud.com>
2017-08-17 10:48:31 +0700

    Update replyComplainform.php

--- replyComplainform.php
+++ replyComplainform.php
@@ -4,7 +4,7 @@
 $access_token = 'nh7BWpnKFdxiz9UTcB3HttsbbBC9DIxMHeQUGznWDqLQ6yAyM9iyYSqn6BO4Yg+6NxNqZA3ZDmcu/1O7RTO0SL4vUdQdoTgOkQCVJP8Qm5O2ivj40/ezbv/n51ekqhaFUGl47j8Yvx8ChjaEhfH/DAdB04t89/1O/w1cDnyilFU=';
 
 //$userId = "U614bbdc142f4207ead2ba6d517aa4ee3";
-$userId = $_POST["userId"];
+$userId = $_POST["user"];
 $subject = $_POST["subject"];
 $detail = $_POST["detail"];
 
