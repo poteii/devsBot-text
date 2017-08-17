8347de9f5c46c6517d418a484c81461adfd5e833
poteii <poteii.pasel@icloud.com>
2017-08-17 10:47:07 +0700

    Update replyComplainform.php

--- replyComplainform.php
+++ replyComplainform.php
@@ -4,9 +4,9 @@
 $access_token = 'nh7BWpnKFdxiz9UTcB3HttsbbBC9DIxMHeQUGznWDqLQ6yAyM9iyYSqn6BO4Yg+6NxNqZA3ZDmcu/1O7RTO0SL4vUdQdoTgOkQCVJP8Qm5O2ivj40/ezbv/n51ekqhaFUGl47j8Yvx8ChjaEhfH/DAdB04t89/1O/w1cDnyilFU=';
 
 //$userId = "U614bbdc142f4207ead2ba6d517aa4ee3";
-$userId = echo $_POST["userId"];
-//$subject = echo $_POST["subject"];
-//$detail = echo $_POST["detail"];
+$userId = $_POST["userId"];
+$subject = $_POST["subject"];
+$detail = $_POST["detail"];
 
 $fburl = 'https://myfirstfirebase-3f424.firebaseio.com/FirstBase.json';
 $fbarr = array($userId => array($subject=> $detail));  
