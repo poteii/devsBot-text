66151054153c4ae636fbfdcad3a8be4cf5b3392f
poteii <poteii.pasel@icloud.com>
2017-08-17 11:07:22 +0700

    Update replyComplainform.php

--- replyComplainform.php
+++ replyComplainform.php
@@ -44,7 +44,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result . "\r\n";
+echo "ขอบคุณสำหรับข้อมูล ทางเราจะดำเนินการโดยเร็วที่สุด" . "\r\n";
 
 
 
