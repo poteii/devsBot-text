4b8add436368308317fe38c5fba2ef6f21002b47
littlesquid <littlesquid.1991@gmail.com>
2017-08-18 09:51:26 +0700

    Update process.php

--- process.php
+++ process.php
@@ -35,6 +35,8 @@ foreach($selected_users as $value){
 }
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 */
+$message = $_POST['msg'];
+echo $message.'<br/>';
 $strAccessToken = 'nh7BWpnKFdxiz9UTcB3HttsbbBC9DIxMHeQUGznWDqLQ6yAyM9iyYSqn6BO4Yg+6NxNqZA3ZDmcu/1O7RTO0SL4vUdQdoTgOkQCVJP8Qm5O2ivj40/ezbv/n51ekqhaFUGl47j8Yvx8ChjaEhfH/DAdB04t89/1O/w1cDnyilFU=';
 //$strAccessToken = "ACCESS_TOKEN";
  
