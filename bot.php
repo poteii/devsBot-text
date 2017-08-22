dee0318725f569b2cdbaa97a71969820de9f5ced
poteii <poteii.pasel@icloud.com>
2017-08-22 15:00:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			if(strstr($event['message']['text'], 'สวัสดี')){
 				// Get text sent
-				$text = "สวัสดีค่ะ คุณ ".$profile[0]->displayName;
+				$text = "สวัสดีค่ะ คุณ ".$profile[0]["displayName"];
 			}else if(strstr($event['message']['text'], 'ร้องเรียน') ||  strstr($event['message']['text'], 'ปัญหา')){
 				$text = "กรุณากรอกรายละเอียดตามนี้ https://devsbottext.herokuapp.com/complainform.php?userId=".$event['source']['userId'];
 			}else{
