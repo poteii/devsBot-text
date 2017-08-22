629bb0c530df0149529eaa549d5da0d52995e7cd
poteii <poteii.pasel@icloud.com>
2017-08-22 15:12:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		$text = "";
 		$replyToken = "";
 		$url = 'https://api.line.me/v2/bot/profile/'.$userId;
-		$temp = file_get_contents($url);
+		$temp = get_remote_data($url);
 		$profile = json_decode($temp, true);
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
